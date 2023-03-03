f65726b00421fa55ebe7973bba2a7baefd4dc684
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-03-03 11:54:33 +0100

    add a ProductRepository method findByCategoryId

--- src/main/java/com/EcommerceSite/dao/ProductRepository.java
+++ src/main/java/com/EcommerceSite/dao/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.EcommerceSite.dao;
 
 import com.EcommerceSite.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product , Long> {
+
+    // SELECT * FROM product where category_id = ?
+    // http://localhost:8080/api/products/search/findCategoryById?id=2
+    //http://localhost:8080/api/product-category/2/products
+    Page<Product> findCategoryById(@Param("id") Long id , Pageable pageable);
 }
