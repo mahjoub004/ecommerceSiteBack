b4ffe79709de77e06445ae3d0de17f5de99f7395
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-03-17 10:46:27 +0100

    create controller for inject the order data in database

---
+++ src/main/java/com/EcommerceSite/CheckoutController/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.EcommerceSite.CheckoutController;
+
+import com.EcommerceSite.dto.Purchase;
+import com.EcommerceSite.dto.PurchaseResponse;
+import com.EcommerceSite.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+
+}
